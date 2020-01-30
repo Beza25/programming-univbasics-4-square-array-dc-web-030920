98ae9efe51958003ed5adf032cb645fd6a0ea7d9
Beza Sirak <sirak.bezawit@gmail.com>
2020-01-30 11:27:56 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,11 +1,11 @@
 def square_array(array)
  counter = 0
- square_array = []
+ new_array = []
  while counter < array.length do
    
-   square_array.push(array[counter] * array[counter])
+   new_array.push(array[counter] * array[counter])
    counter += 1
-   puts square_array
-   return square_array
+   puts new_array
+   return new_array
  end
 end
